0f03d805b9652902cf5e38930e76d7160b2a255f
cryptarbitrage-code <cryptosheetsharing+github@gmail.com>
2024-07-04 18:09:57 +0200

    added data imports

--- api_functions.py
+++ api_functions.py
@@ -2,8 +2,9 @@ import hashlib
 import hmac
 import json
 import time
+import pandas as pd
 import requests
-from settings import *
+from settings_backup import *
 
 
 def get_positions():
@@ -13,7 +14,7 @@ def get_positions():
     # nonce should be a random string, static nonce for simple illustration here
     nonce = "1234567890"
     # body of the POST request, JSON-RPC string
-    body = "{\"jsonrpc\": \"2.0\",\"id\": \"8003\",\"method\": \"private/get_positions\",\"params\": {\"currency\": \"BTC\"}}"
+    body = "{\"jsonrpc\": \"2.0\",\"id\": \"8003\",\"method\": \"private/get_positions\"}"
 
     # signature for the request
     request_data = "POST" + "\n" + url + "\n" + body + "\n"
@@ -29,57 +30,10 @@ def get_positions():
     json_response = requests.post((api_exchange_address + url + "?"), headers=headers, data=body)
     response_dict = json.loads(json_response.content)
 
-    positions_list_BTC = response_dict["result"]
+    positions_list = response_dict["result"]
+    df_positions = pd.DataFrame(positions_list)
 
-    # ETH POSITIONS
-    tstamp = str(int(time.time()) * 1000)
-    url = "/api/v2/private/get_positions"
-    # nonce should be a random string, static nonce for simple illustration here
-    nonce = "1234567891"
-    # body of the POST request, JSON-RPC string
-    body = "{\"jsonrpc\": \"2.0\",\"id\": \"8003\",\"method\": \"private/get_positions\",\"params\": {\"currency\": \"ETH\"}}"
-
-    # signature for the request
-    request_data = "POST" + "\n" + url + "\n" + body + "\n"
-    base_signature_string = tstamp + "\n" + nonce + "\n" + request_data
-    byte_key = api_client_secret.encode()
-    message = base_signature_string.encode()
-    sig = hmac.new(byte_key, message, hashlib.sha256).hexdigest()
-
-    authorization = "deri-hmac-sha256 id=" + api_client_id + ",ts=" + tstamp + ",sig=" + sig + ",nonce=" + nonce
-    headers = {"Authorization": authorization}
-
-    # send HTTPS POST request
-    json_response = requests.post((api_exchange_address + url + "?"), headers=headers, data=body)
-    response_dict = json.loads(json_response.content)
-
-    positions_list_ETH = response_dict["result"]
-
-    # SOL POSITIONS
-    tstamp = str(int(time.time()) * 1000)
-    url = "/api/v2/private/get_positions"
-    # nonce should be a random string, static nonce for simple illustration here
-    nonce = "1234567892"
-    # body of the POST request, JSON-RPC string
-    body = "{\"jsonrpc\": \"2.0\",\"id\": \"8003\",\"method\": \"private/get_positions\",\"params\": {\"currency\": \"SOL\"}}"
-
-    # signature for the request
-    request_data = "POST" + "\n" + url + "\n" + body + "\n"
-    base_signature_string = tstamp + "\n" + nonce + "\n" + request_data
-    byte_key = api_client_secret.encode()
-    message = base_signature_string.encode()
-    sig = hmac.new(byte_key, message, hashlib.sha256).hexdigest()
-
-    authorization = "deri-hmac-sha256 id=" + api_client_id + ",ts=" + tstamp + ",sig=" + sig + ",nonce=" + nonce
-    headers = {"Authorization": authorization}
-
-    # send HTTPS POST request
-    json_response = requests.post((api_exchange_address + url + "?"), headers=headers, data=body)
-    response_dict = json.loads(json_response.content)
-
-    positions_list_SOL = response_dict["result"]
-
-    return positions_list_BTC, positions_list_ETH, positions_list_SOL
+    return df_positions
 
 
 def get_instrument(instrument):
@@ -124,3 +78,17 @@ def get_order_book(instrument):
     instrument_details = response_dict["result"]
 
     return instrument_details
+
+def get_tradingview_chart_data(instrument_name, start_timestamp, end_timestamp, resolution):
+    url = "/api/v2/public/get_tradingview_chart_data"
+    parameters = {
+        'instrument_name': instrument_name,
+        'start_timestamp': start_timestamp,
+        'end_timestamp': end_timestamp,
+        'resolution': resolution,
+    }
+    # send HTTPS GET request
+    json_response = requests.get((api_exchange_address + url + "?"), params=parameters)
+    response_dict = json.loads(json_response.content)
+    candle_details = response_dict["result"]
+    return candle_details
--- main.py
+++ main.py
@@ -1,41 +1,61 @@
 import plotly.graph_objs as go
 from dash import Dash, dcc, html
 import pandas as pd
+from api_functions import get_tradingview_chart_data, get_positions
+from datetime import datetime, timedelta
+
+pd.set_option('display.max_columns', None)  # useful for testing
+
+# --- GET CANDLE DATA --- #
+instrument = "BTC-PERPETUAL"
+resolution = 720  # resolution in minutes
+end_time = datetime.now()
+start_time = end_time - timedelta(minutes=20 * resolution)
+
+# Convert start_timestamp and end_timestamp to unix time in milliseconds
+start_timestamp = int(start_time.timestamp() * 1000)
+end_timestamp = int(end_time.timestamp() * 1000)
+
+candle_data = get_tradingview_chart_data(instrument, start_timestamp, end_timestamp, resolution)
+df_candle = pd.DataFrame(candle_data)
+print(df_candle)
+df_candle = df_candle[['ticks', 'open', 'high', 'low', 'close']]
+df_candle['date'] = pd.to_datetime(df_candle['ticks'], unit='ms')
+
+# --- GET POSITION DATA --- #
+positions = get_positions()
+
+columns_to_keep = [
+    'average_price_usd', 'total_profit_loss', 'average_price', 'theta', 'delta', 'vega',
+    'instrument_name', 'mark_price', 'direction', 'size', 'kind', 'index_price'
+]
+positions = positions[columns_to_keep]
+
+option_positions = positions[positions['kind'] == "option"].copy()
+option_positions[['currency', 'expiry', 'strike', 'type']] = option_positions['instrument_name'].str.split('-', expand=True)
+option_positions = option_positions[option_positions['currency'] == "BTC"]
+
+### need to create column for expiry_date
+
+print(option_positions)
 
 app = Dash(__name__)
 
-# Sample data
-data = {
-    'date': ['2021-07-01', '2021-07-02', '2021-07-03', '2021-07-04', '2021-07-05'],
-    'open': [100, 105, 103, 108, 107],
-    'high': [110, 115, 113, 118, 117],
-    'low': [90, 95, 93, 98, 97],
-    'close': [105, 103, 108, 107, 110]
-}
-
 # Create the candlestick chart
-fig = go.Figure(data=[go.Candlestick(x=data['date'],
-                                     open=data['open'],
-                                     high=data['high'],
-                                     low=data['low'],
-                                     close=data['close'])])
+fig = go.Figure(data=[go.Candlestick(x=df_candle['date'],
+                                     open=df_candle['open'],
+                                     high=df_candle['high'],
+                                     low=df_candle['low'],
+                                     close=df_candle['close'])])
 
-# Example option positions with dates beyond the last candle date
-positions = [
-    {'date': '2021-07-06', 'type': 'call', 'action': 'long', 'price': 112},
-    {'date': '2021-07-07', 'type': 'put', 'action': 'long', 'price': 105},
-    {'date': '2021-07-08', 'type': 'call', 'action': 'sell', 'price': 115},
-    {'date': '2021-07-09', 'type': 'put', 'action': 'sell', 'price': 108}
-]
-
-# Add arrows or triangles for each position
-for pos in positions:
-    arrow_color = 'green' if pos['action'] == 'long' else 'red'
-    y_offset = 5 if pos['type'] == 'call' else -5
-    symbol = 'triangle-up' if pos['type'] == 'call' else 'triangle-down'
+# Add arrows for each position
+for index, pos in option_positions.iterrows():
+    arrow_color = 'green' if pos['direction'] == 'buy' else 'red'
+    y_offset = 5 if pos['type'] == 'C' else -5
+    symbol = 'triangle-up' if pos['type'] == 'C' else 'triangle-down'
     fig.add_trace(go.Scatter(
-        x=[pos['date']],
-        y=[pos['price']],
+        x=[pos['expiry_date']],
+        y=[pos['strike']],
         mode='markers',  # markers+text to include text
         marker=dict(
             symbol=symbol,
@@ -43,7 +63,7 @@ for pos in positions:
             size=12
         ),
         # text=pos['type'].capitalize(),
-        textposition="top center"
+        # textposition="top center"
     ))
 
 # Adjust layout settings
